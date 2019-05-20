8d034ccc3504570811dbe93053f5c5b2524c58c5
Thomas Zhong <39574072+thomaszhong@users.noreply.github.com>
2019-05-20 08:41:17 +0800

    Add Comment

--- android-app/app/src/main/java/com/thomas/productsearch/SplashScreen.java
+++ android-app/app/src/main/java/com/thomas/productsearch/SplashScreen.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+Initial splash screen when application starts
+*/
 public class SplashScreen extends AppCompatActivity {
 
     /** Duration of wait **/
